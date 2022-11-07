88a84b95270b327b74647ab15f20c57eeac8694d
3takita <98113361+3takita@users.noreply.github.com>
2022-11-07 00:03:57 -0800

    Update Menu.h

--- Menu.h
+++ Menu.h
@@ -1,48 +1,34 @@
+//File: Menu.h
 #ifndef MENU_H
 #define MENU_H
 #include <iostream>
 using namespace std;
 
-void init_mainMenu(); 
-double inputInteger(string prompt, double input); 
-double num1(); 
-double num2(); 
-int menuOption(); 
+void init_mainMenu();
+double inputNum(string prompt, double input);
+double num1();
+double num2();
+int menuOption();
 
-class Math { 
+class Math {
 private:
-	double m, n;
+	double m;
+	double n;
 public:
 	//constructor(s)
 	Math();
-	Math(const double& _m, const double& _n);
-	//setter(s)
+	//setters
 	void setM(const double& _m);
 	void setN(const double& _n);
-	//getter(s)
+	//getters
 	double getN();
 	double getM();
-	  /* member functions */
-	
-    //Preconditon: Takes two numbers as argument
-    //Postcondition: Prints the sum of the numbers
-    void add();
-
-    //Preconditon: Takes two numbers as argument
-    //Postcondition: Prints the difference between the numbers to the screen
-    void subtract();
-
-    //Preconditon: Takes two numbers as argument
-    //Postcondition: Prints their product to the screen
-    void multiply();
-
-    //Preconditon: Takes two numbers as argument
-    //Postcondition: Prints their quotient to the screen
-    void divide();
-
-    //Preconditon: Takes two numbers as argument
-    //Postcondition: Prints the first number raised to the power of the second number to the schreen
-    void exponent();
+	//member functions
+	void add();
+	void subtract();
+	void multiply();
+	void divide();
+	void exponent();
 };
 
-#endif 
+#endif
